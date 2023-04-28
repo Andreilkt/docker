f198a5021f3976e877acdacd30702160bcc071be
andrey <andreilkt@yandex.ru>
2023-04-28 11:33:47 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#apt install docker docker.io
+#systemstl enable docker
+#systemctl start docker
+# docker run hello-world
+# systemctl status docker
+
+#docker login -u savchukandrey777
+#dckr_pat_NRl0BE9422KQA33fjPTPWSRTlIA
+
+
+
